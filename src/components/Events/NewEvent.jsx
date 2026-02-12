50839737a2b3d4dcc56e56aac8b2a4299f309bd6
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-12 12:29:03 +0530

    feat : post data logic add for create event

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,42 @@
-import { Link, useNavigate } from 'react-router-dom';
-
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import ErrorBlock from "../../components/UI/ErrorBlock.jsx";
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { createNewEvent } from "../../util/http.js";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && <p>Submitting...</p>}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Failed To Create An Event!"
+          message={error.info?.message || "Can't Submit This Event..."}
+        />
+      )}
     </Modal>
   );
 }
